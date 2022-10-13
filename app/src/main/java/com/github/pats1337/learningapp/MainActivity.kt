5fa55caf34b0d8790a2e87b0d13c9b604bfce184
Alex Terentev <Ater1337@yandex.ru>
2022-10-13 12:41:48 +0300

    Testing spannableString's

--- app/src/main/java/com/github/pats1337/learningapp/MainActivity.kt
+++ app/src/main/java/com/github/pats1337/learningapp/MainActivity.kt
@@ -4,22 +4,69 @@ import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.view.View
 import android.widget.TextView
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val titleTextView = findViewById<TextView>(R.id.titleTextView)
-        val string = "This is title from code!"
-        titleTextView.text = string
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-            titleTextView.setTextColor(resources.getColor(R.color.black, null))
-        }else{
-            titleTextView.setTextColor(resources.getColor(R.color.black))
+        val agreementTextView: TextView = findViewById(R.id.agreementTextView)
+
+        val fullText = getString(R.string.agreement_full_text)
+        val confidential = getString(R.string.confidential_info)
+        val policy = getString(R.string.privacy_policy)
+        val spannableString = SpannableString(fullText)
+
+        val confidentialClickable = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                Snackbar.make(widget, "Go to link1", Snackbar.LENGTH_SHORT).show()
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText = true
+                ds.color = Color.parseColor("#FF0000")
+            }
         }
-        titleTextView.visibility = View.INVISIBLE
+
+        val policyClickable = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                Snackbar.make(widget, "Go to link2", Snackbar.LENGTH_SHORT).show()
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText = true
+                ds.color = Color.parseColor("#FF0000")
+            }
+        }
+
+        spannableString.setSpan(
+            confidentialClickable,
+            fullText.indexOf(confidential),
+            fullText.indexOf(confidential) + confidential.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        spannableString.setSpan(
+            policyClickable,
+            fullText.indexOf(policy),
+            fullText.indexOf(policy) + policy.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        agreementTextView.run {
+            text = spannableString
+            movementMethod = LinkMovementMethod.getInstance()
+            highlightColor = Color.BLUE
+        }
     }
 }
