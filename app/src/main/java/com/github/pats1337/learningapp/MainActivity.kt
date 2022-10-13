41b03d52077d90e51ed627c165dd735d5a7394fc
Alex Terentev <Ater1337@yandex.ru>
2022-10-13 12:03:19 +0300

    Testing text output from code

--- app/src/main/java/com/github/pats1337/learningapp/MainActivity.kt
+++ app/src/main/java/com/github/pats1337/learningapp/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.github.pats1337.learningapp
 
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val titleTextView = findViewById<TextView>(R.id.titleTextView)
+        val string = "This is title from code!"
+        titleTextView.text = string
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            titleTextView.setTextColor(resources.getColor(R.color.black, null))
+        }else{
+            titleTextView.setTextColor(resources.getColor(R.color.black))
+        }
+        titleTextView.visibility = View.INVISIBLE
     }
 }
