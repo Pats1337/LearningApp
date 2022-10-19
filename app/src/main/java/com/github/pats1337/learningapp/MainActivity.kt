759f5427d49323a2da86e2e0c98cb7f70b88d128
Alex Terentev <Ater1337@yandex.ru>
2022-10-19 11:57:07 +0300

    Testing drawables

--- app/src/main/java/com/github/pats1337/learningapp/MainActivity.kt
+++ app/src/main/java/com/github/pats1337/learningapp/MainActivity.kt
@@ -17,56 +17,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val agreementTextView: TextView = findViewById(R.id.agreementTextView)
-
-        val fullText = getString(R.string.agreement_full_text)
-        val confidential = getString(R.string.confidential_info)
-        val policy = getString(R.string.privacy_policy)
-        val spannableString = SpannableString(fullText)
-
-        val confidentialClickable = object : ClickableSpan() {
-            override fun onClick(widget: View) {
-                Snackbar.make(widget, "Go to link1", Snackbar.LENGTH_SHORT).show()
-            }
-
-            override fun updateDrawState(ds: TextPaint) {
-                super.updateDrawState(ds)
-                ds.isUnderlineText = true
-                ds.color = Color.parseColor("#FF0000")
-            }
-        }
-
-        val policyClickable = object : ClickableSpan() {
-            override fun onClick(widget: View) {
-                Snackbar.make(widget, "Go to link2", Snackbar.LENGTH_SHORT).show()
-            }
-
-            override fun updateDrawState(ds: TextPaint) {
-                super.updateDrawState(ds)
-                ds.isUnderlineText = true
-                ds.color = Color.parseColor("#FF0000")
-            }
-        }
-
-        spannableString.setSpan(
-            confidentialClickable,
-            fullText.indexOf(confidential),
-            fullText.indexOf(confidential) + confidential.length,
-            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
-        )
-
-        spannableString.setSpan(
-            policyClickable,
-            fullText.indexOf(policy),
-            fullText.indexOf(policy) + policy.length,
-            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
-        )
-
-        agreementTextView.run {
-            text = spannableString
-            movementMethod = LinkMovementMethod.getInstance()
-            highlightColor = Color.BLUE
-        }
     }
 }
