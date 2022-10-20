f65e85d04e61a42a1a40675a90afb3a014ed0938
Alex Terentev <Ater1337@yandex.ru>
2022-10-20 09:54:06 +0300

    Testing InputLayout

--- app/src/main/java/com/github/pats1337/learningapp/MainActivity.kt
+++ app/src/main/java/com/github/pats1337/learningapp/MainActivity.kt
@@ -17,25 +17,10 @@ import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
-    private companion object {
-        const val URL =
-            "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiWj4C2qWdTfHtZS11SKHgE2rae404ImVR7gaXjZarO9aUYkvJR0LNbzR3r2OWajQIlu2W7_b7xz09lw_bqLVLwLBG_BcuZzg0s6nkJ8UIxzAz0ETcoAJVseLURltDI_rhhBSmcQ5Xu4FyeCR5mfZO1WQ-WZUNPCzBkxH96vAezjp3Szyb_8cqVIGip/w400-h400/image1.png"
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val image = findViewById<ImageView>(R.id.ImageView)
-        val netImage = NetImage(URL, object : ImageCallback {
-            override fun success(bitmap: Bitmap) {
-                image.setImageBitmap(bitmap)
-            }
-
-            override fun failed() {
-                Snackbar.make(image, "failed", Snackbar.LENGTH_SHORT).show()
-            }
-        })
-        netImage.start()
     }
+
 }
