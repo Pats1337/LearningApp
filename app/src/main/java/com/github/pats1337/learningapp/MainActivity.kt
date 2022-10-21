aaafd5fb667955699f75fc3f36d0eb59a4658d4e
Alex Terentev <Ater1337@yandex.ru>
2022-10-21 11:48:24 +0300

    Testing email validation

--- app/src/main/java/com/github/pats1337/learningapp/MainActivity.kt
+++ app/src/main/java/com/github/pats1337/learningapp/MainActivity.kt
@@ -4,7 +4,12 @@ package com.github.pats1337.learningapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.util.Patterns.EMAIL_ADDRESS
+import android.widget.Button
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 import com.google.android.material.textfield.TextInputLayout
@@ -16,20 +21,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val textInputLayout = findViewById<TextInputLayout>(R.id.textInputLayout)
-        val textInputEditText = textInputLayout.editText as TextInputEditText
+        val textInputEditText = findViewById<TextInputEditText>(R.id.textInputEditText)
 
-        textInputEditText.addTextChangedListener(object : SimpleTextWatcher() {
-            override fun afterTextChanged(p0: Editable?) {
-                val valid = android.util.Patterns.EMAIL_ADDRESS.matcher(p0.toString()).matches()
-                textInputLayout.isErrorEnabled = !valid
-                val error = if (valid) "" else getString(R.string.invalid_email_message)
-                textInputLayout.error = error
-                if (valid) Toast.makeText(
-                    this@MainActivity,
-                    R.string.valid_email_message,
-                    Toast.LENGTH_LONG
-                ).show()
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        loginButton.setOnClickListener {
+            if (EMAIL_ADDRESS.matcher(textInputEditText.text.toString()).matches()) {
+                Snackbar.make(loginButton, "Go to post login", Snackbar.LENGTH_LONG).show()
+            } else {
+                textInputLayout.isErrorEnabled = true
+                textInputLayout.error = getString(R.string.invalid_email_message)
+            }
+        }
+
+        textInputEditText.listenChanges { textInputLayout.isErrorEnabled = false }
+
+    }
+
+    fun TextInputEditText.listenChanges(block: (text: String) -> Unit) {
+        addTextChangedListener(object : SimpleTextWatcher() {
+            override fun afterTextChanged(s: Editable?) {
+                block.invoke(s.toString())
             }
         })
     }
 }
+
--- app/src/main/java/com/github/pats1337/learningapp/SimpleTextWatcher.kt
+++ app/src/main/java/com/github/pats1337/learningapp/SimpleTextWatcher.kt
@@ -5,12 +5,12 @@ import android.text.TextWatcher
 
 abstract class SimpleTextWatcher : TextWatcher {
 
-    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
     }
 
-    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+    override fun onTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
     }
 
-    override fun afterTextChanged(p0: Editable?) {
+    override fun afterTextChanged(s: Editable?) {
     }
 }
